86974bbec719352c689cc562c4581bc27eebb5c7
aka-abdol <thegrolam@gmail.com>
2023-12-07 08:21:56 +0330

    feat: swagger configured

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('bluJunior swagger')
+    .setDescription('api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
